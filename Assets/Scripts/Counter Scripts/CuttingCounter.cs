35974328de57054b0c8a1a75889bddab485fbcbb
Teglas Dennis <teglasdennis@yahoo.com>
2024-12-07 17:35:21 +0200

    Restricted Player to placing only cuttable food on cutting counters

--- Assets/Scripts/Counter Scripts/CuttingCounter.cs
+++ Assets/Scripts/Counter Scripts/CuttingCounter.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CuttingCounter : ClearCounter
+public class CuttingCounter : BaseCounter
 {
     [SerializeField] private List<CuttableFoodSO> cuttableFoods;
 
@@ -13,5 +13,24 @@ public class CuttingCounter : ClearCounter
     void Update()
     {
         
+    }
+
+    public override void Interact(Player player)
+    {
+        if (!HasKitchenObject() && Player.Instance.HasKitchenObject())
+        {
+            if (cuttableFoods.Find(cuttableFood => cuttableFood.originalFood.objectName == Player.Instance.GetKitchenObject().GetKitchenObjectSO().objectName)) 
+            {
+                SetKitchenObject(Player.Instance.GetKitchenObject());
+                GetKitchenObject().SetKitchenObjectParent(this);
+            }
+        }
+        else if (!Player.Instance.HasKitchenObject() && HasKitchenObject())
+        {
+            if (Player.Instance.SetKitchenObject(GetKitchenObject())) 
+            {
+                Player.Instance.GetKitchenObject().SetKitchenObjectParent(Player.Instance);
+            }
+        }
     }
 }
