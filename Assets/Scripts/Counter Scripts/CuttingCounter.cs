f9d9d417c2c7ed83b5afd56d44c277b56e035e2f
Vlad-Stefan Sas <me@svs.pm>
2024-12-07 12:36:37 +0200

    Added CuttingCounter visuals and basic functionality

---
+++ Assets/Scripts/Counter Scripts/CuttingCounter.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CuttingCounter : ClearCounter
+{
+    [SerializeField] private List<CuttableFoodSO> cuttableFoods;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
